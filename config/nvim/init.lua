4eb6c95a96bc54800f5f95d99e7dc50d2149808d
Øyvind Aarrestad Aakre <oyvind.aakre@easee.com>
2024-04-23 14:07:08 +0200

    [nvim dap] setup DAP for debugging vscode projects

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -909,6 +909,66 @@ require("lazy").setup({
 			})
 		end,
 	},
+	--
+	--
+	-- DAP configuration
+	{
+		"mfussenegger/nvim-dap", -- DAP client
+
+		dependencies = {
+			-- Creates a beautiful debugger UI
+			-- 'rcarriga/nvim-dap-ui',
+
+			-- Required dependency for nvim-dap-ui
+			-- 'nvim-neotest/nvim-nio',
+
+			-- Installs the debug adapters for you
+			"williamboman/mason.nvim",
+			"jay-babu/mason-nvim-dap.nvim",
+
+			-- Add your own debuggers here
+			-- Personally prefer to use Mason to install Marus/cortex-debug". If installing from here then make sure to set 'extension_path' to where lazy installed cortex-debug
+			"jedrzejboczar/nvim-dap-cortex-debug", -- An extension for nvim-dap providing integration with Marus/cortex-debug debug adapter.
+		},
+		config = function()
+			local dap = require("dap")
+			dap.set_log_level("DEBUG")
+			-- Set up and register with nvim-dap
+			require("dap-cortex-debug").setup({
+				debug = false, -- log debug messages
+				-- path to cortex-debug extension, supports vim.fn.glob
+				-- by default tries to guess: mason.nvim or VSCode extensions
+				extension_path = nil,
+				lib_extension = nil, -- shared libraries extension, tries auto-detecting, e.g. 'so' on unix
+				node_path = "node", -- path to node.js executable
+				dapui_rtt = true, -- register nvim-dap-ui RTT element
+				-- make :DapLoadLaunchJSON register cortex-debug for C/C++, set false to disable
+				dap_vscode_filetypes = { "c", "cpp", "netrw", "h" }, -- Added "netrw" so that you can get to the debugger menu from most places (i.e. not having to be in a c/h file)
+			})
+
+			-- Hack that replaces '${workspaceRoot}' in the launch.json files with cwd so that the elf files are found by the debugger
+			-- Note that this will only work when launching nvim from the root directory of the project, so it's brittle
+			require("dap.ext.vscode").load_launchjs() -- load .vsocde/launch.json
+
+			local c_table = dap.configurations.c
+			for _, v in pairs(c_table) do
+				v["cwd"] = vim.fn.getcwd() -- replaces ${workspaceRoot} with cwd
+			end
+
+			dap.configurations.c = c_table -- back configuration to dap
+
+			-- Debugger keymap
+			vim.keymap.set("n", "<F5>", dap.continue, { desc = "Debug: Start/Continue" })
+			vim.keymap.set("n", "<F1>", dap.step_into, { desc = "Debug: Step Into" })
+			vim.keymap.set("n", "<F2>", dap.step_over, { desc = "Debug: Step Over" })
+			vim.keymap.set("n", "<F3>", dap.step_out, { desc = "Debug: Step Out" })
+			vim.keymap.set("n", "<leader>b", dap.toggle_breakpoint, { desc = "Debug: Toggle Breakpoint" })
+			vim.keymap.set("n", "<leader>B", function()
+				dap.set_breakpoint(vim.fn.input("Breakpoint condition: "))
+			end, { desc = "Debug: Set Breakpoint" })
+		end,
+	},
+
 	-- NOTE: The import below can automatically add your own plugins, configuration, etc from `lua/custom/plugins/*.lua`
 	--    This is the easiest way to modularize your config.
 	--
