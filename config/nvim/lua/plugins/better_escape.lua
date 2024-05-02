d9ec83a96503945924522a90ad8f49896b9607aa
Øyvind Aarrestad Aakre <oyvind.aakre@easee.com>
2024-05-02 20:11:40 +0200

    [better escape] remove kj and kk keybindings, bad choice when writing in norwegian

--- config/nvim/lua/plugins/better_escape.lua
+++ config/nvim/lua/plugins/better_escape.lua
@@ -2,7 +2,7 @@ return {
 	"max397574/better-escape.nvim",
 	config = function()
 		require("better_escape").setup({
-			mapping = { "jk", "jj", "kj", "kk" }, -- a table with mappings to use
+			mapping = { "jk", "jj" }, -- a table with mappings to use
 			timeout = 500, -- the time in which the keys must be hit in ms. Use option timeoutlen by default
 			clear_empty_lines = false, -- clear line after escaping if there is only whitespace
 			keys = "<Esc>", -- keys used for escaping, if it is a function will use the result everytime
