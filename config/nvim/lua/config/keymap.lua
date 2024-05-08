6302737412d0a69a2ff3f88d0585a9236566ac45
Øyvind Aarrestad Aakre <oyvind.aakre@easee.com>
2024-05-08 15:02:48 +0200

    Move lines up and down with Alt+j/k

--- config/nvim/lua/config/keymap.lua
+++ config/nvim/lua/config/keymap.lua
@@ -32,3 +32,5 @@ vim.keymap.set("n", "<C-h>", "<C-w><C-h>", { desc = "Move focus to the left wind
 vim.keymap.set("n", "<C-l>", "<C-w><C-l>", { desc = "Move focus to the right window" })
 vim.keymap.set("n", "<C-j>", "<C-w><C-j>", { desc = "Move focus to the lower window" })
 vim.keymap.set("n", "<C-k>", "<C-w><C-k>", { desc = "Move focus to the upper window" })
+vim.keymap.set("n", "<A-j>", ":m .+1<CR>==", { desc = "Move line up(n)" })
+vim.keymap.set("n", "<A-k>", ":m .-2<CR>==", { desc = "Move line down(n)" })
