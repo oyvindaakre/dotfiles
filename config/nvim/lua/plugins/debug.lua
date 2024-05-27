a8368a6871a957a10d640e8e4d326b5bca094c3e
Øyvind Aarrestad Aakre <oyvind.aakre@easee.com>
2024-05-27 10:22:43 +0200

    [nvim debug] Update keymap

--- config/nvim/lua/plugins/debug.lua
+++ config/nvim/lua/plugins/debug.lua
@@ -26,10 +26,10 @@ return {
 		local debug_filetypes = { "c", "cpp", "netrw", "h" }
 
 		-- Debug keymap
-		vim.keymap.set("n", "<F1>", dap.step_into, { desc = "Debug: Step Into" })
+		vim.keymap.set("n", "<F3>", dap.step_into, { desc = "Debug: Step Into" })
 		vim.keymap.set("n", "<F2>", dap.step_over, { desc = "Debug: Step Over" })
-		vim.keymap.set("n", "<F3>", dap.step_out, { desc = "Debug: Step Out" })
-		vim.keymap.set("n", "<F5>", dap.continue, { desc = "Debug: Start/Continue" })
+		vim.keymap.set("n", "<F4>", dap.step_out, { desc = "Debug: Step Out" })
+		vim.keymap.set("n", "<F1>", dap.continue, { desc = "Debug: Start/Continue" })
 		-- Toggle to see last session result. Without this, you can't see session output in case of unhandled exception.
 		vim.keymap.set("n", "<F7>", dapui.toggle, { desc = "Debug: See last session result." })
 		vim.keymap.set("n", "<leader>b", dap.toggle_breakpoint, { desc = "Debug: Toggle Breakpoint" })
