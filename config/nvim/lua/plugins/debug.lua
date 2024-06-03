31c1b218260a6abe08bf41510c5d3bb4dd896acf
Øyvind Aarrestad Aakre <oyvind.aakre@easee.com>
2024-06-03 12:32:40 +0200

    [nvim debug] Start debugging with F5 as well
    Debugging can be started with
    F1
    F5
    <leader>dc

--- config/nvim/lua/plugins/debug.lua
+++ config/nvim/lua/plugins/debug.lua
@@ -32,6 +32,7 @@ return {
 		vim.keymap.set("n", "<F2>", dap.step_over, { desc = "Debug: Step Over" })
 		vim.keymap.set("n", "<F4>", dap.step_out, { desc = "Debug: Step Out" })
 		vim.keymap.set("n", "<F1>", dap.continue, { desc = "Debug: Start/Continue" })
+		vim.keymap.set("n", "<F5>", dap.continue, { desc = "Debug: Start/Continue" })
 		-- Toggle to see last session result. Without this, you can't see session output in case of unhandled exception.
 		vim.keymap.set("n", "<F7>", dapui.toggle, { desc = "Debug: See last session result." })
 		vim.keymap.set("n", "<leader>b", dap.toggle_breakpoint, { desc = "Debug: Toggle Breakpoint" })
