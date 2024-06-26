5b2fee79d33f7dcf9658d392a7b774ab9d99a8bd
Øyvind Aarrestad Aakre <oyvind.aakre@easee.com>
2024-06-26 08:33:04 +0200

    Configure identomatic

--- config/nvim/lua/plugins/indentomatic.lua
+++ config/nvim/lua/plugins/indentomatic.lua
@@ -1,3 +1,20 @@
 return {
-	"Darazaki/indent-o-matic", -- Detect tabstop and shiftwidth automatically
+  "Darazaki/indent-o-matic", -- Detect tabstop and shiftwidth automatically
+  config = function()
+    require("indent-o-matic").setup({
+      -- Global settings (optional, used as fallback)
+      max_lines = 2048,
+      standard_widths = { 2, 4, 8 },
+
+      -- Only detect 4 spaces and tabs for Rust files
+      filetype_lua = {
+        standard_widths = { 2 },
+      },
+
+      -- Don't detect 8 spaces indentations inside files without a filetype
+      filetype_ = {
+        standard_widths = { 2, 4 },
+      },
+    })
+  end,
 }
