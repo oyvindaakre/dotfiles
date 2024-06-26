35593bf4082bd27ec6e736e7aa79bab5cac1e6d1
Øyvind Aarrestad Aakre <oyvind.aakre@easee.com>
2024-06-26 08:32:44 +0200

    Use criterion branch of quicktest

--- config/nvim/lua/plugins/quicktest.lua
+++ config/nvim/lua/plugins/quicktest.lua
@@ -1,70 +1,87 @@
 return {
-	"quolpr/quicktest.nvim",
-	config = function()
-		local qt = require("quicktest")
+  "quolpr/quicktest.nvim",
+  config = function()
+    local qt = require("quicktest")
 
-		qt.setup({
-			-- Choose your adapter, here all supported adapters are listed
-			adapters = {
-				require("quicktest.adapters.golang"),
-				require("quicktest.adapters.vitest"),
-				require("quicktest.adapters.criterion"),
-			},
-		})
-	end,
-	dependencies = {
-		"nvim-lua/plenary.nvim",
-		"MunifTanjim/nui.nvim",
-		"m00qek/baleia.nvim",
-	},
-	keys = {
-		{
-			"<leader>tr",
-			function()
-				local qt = require("quicktest")
-				-- current_win_mode return currently opened panel, split or popup
-				qt.run_line()
-				-- You can force open split or popup like this:
-				-- qt().run_current('split')
-				-- qt().run_current('popup')
-			end,
-			desc = "[T]est [R]un",
-		},
-		{
-			"<leader>tR",
-			function()
-				local qt = require("quicktest")
+    qt.setup({
+      -- Choose your adapter, here all supported adapters are listed
+      adapters = {
+        require("quicktest.adapters.golang"),
+        require("quicktest.adapters.vitest"),
+        require("quicktest.adapters.criterion")({
+          builddir = function(bufnr)
+            local bufname = vim.api.nvim_buf_get_name(bufnr)
+            local builddir
+            if string.find(bufname, "quicktest.nvim") ~= nil then
+              builddir = "tests/support/criterion/build"
+            else
+              builddir = "build"
+            end
+            print("bufnr: " .. tostring(bufnr))
+            print("bufname: " .. bufname)
+            print("using builddir: " .. builddir)
+            return builddir
+          end,
+          additional_args = function(bufnr)
+            return { "--timeout=1" }
+          end,
+        }),
+      },
+    })
+  end,
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    "MunifTanjim/nui.nvim",
+    "m00qek/baleia.nvim",
+  },
+  keys = {
+    {
+      "<leader>tr",
+      function()
+        local qt = require("quicktest")
+        -- current_win_mode return currently opened panel, split or popup
+        qt.run_line()
+        -- You can force open split or popup like this:
+        -- qt().run_current('split')
+        -- qt().run_current('popup')
+      end,
+      desc = "[T]est [R]un",
+    },
+    {
+      "<leader>tR",
+      function()
+        local qt = require("quicktest")
 
-				qt.run_file()
-			end,
-			desc = "[T]est [R]un file",
-		},
-		{
-			"<leader>tp",
-			function()
-				local qt = require("quicktest")
+        qt.run_file()
+      end,
+      desc = "[T]est [R]un file",
+    },
+    {
+      "<leader>tp",
+      function()
+        local qt = require("quicktest")
 
-				qt.run_previous()
-			end,
-			desc = "[T]est Run [P]revious",
-		},
-		{
-			"<leader>tt",
-			function()
-				local qt = require("quicktest")
+        qt.run_previous()
+      end,
+      desc = "[T]est Run [P]revious",
+    },
+    {
+      "<leader>tt",
+      function()
+        local qt = require("quicktest")
 
-				qt.toggle_win("popup")
-			end,
-			desc = "[T]est [T]oggle popup window",
-		},
-		{
-			"<leader>ts",
-			function()
-				local qt = require("quicktest")
+        qt.toggle_win("popup")
+      end,
+      desc = "[T]est [T]oggle popup window",
+    },
+    {
+      "<leader>ts",
+      function()
+        local qt = require("quicktest")
 
-				qt.toggle_win("split")
-			end,
-			desc = "[T]est Toggle [S]plit window",
-		},
-	},
+        qt.toggle_win("split")
+      end,
+      desc = "[T]est Toggle [S]plit window",
+    },
+  },
 }
