51bb1d933f3c65346dd23522cb61eadd43edf21f
Øyvind Aarrestad Aakre <oyvind.aakre@easee.com>
2024-08-07 10:49:02 +0200

    Add relics from testing auto command to set cindent (commented out)
    Adding here in case I want to revisit later

--- config/nvim/lua/config/autocmd.lua
+++ config/nvim/lua/config/autocmd.lua
@@ -4,9 +4,18 @@
 --  Try it with `yap` in normal mode
 --  See `:help vim.highlight.on_yank()`
 vim.api.nvim_create_autocmd("TextYankPost", {
-	desc = "Highlight when yanking (copying) text",
-	group = vim.api.nvim_create_augroup("kickstart-highlight-yank", { clear = true }),
-	callback = function()
-		vim.highlight.on_yank()
-	end,
+  desc = "Highlight when yanking (copying) text",
+  group = vim.api.nvim_create_augroup("kickstart-highlight-yank", { clear = true }),
+  callback = function()
+    vim.highlight.on_yank()
+  end,
 })
+
+-- vim.api.nvim_create_autocmd({ "BufEnter" }, {
+--   pattern = { "*.c", "*h" },
+--   group = vim.api.nvim_create_augroup("custom-indent", { clear = true }),
+--   -- callback = function(ev)
+--   --   print(string.format("event fired: %s", vim.inspect(ev)))
+--   -- end,
+--   command = "set cindent shiftwidth=4",
+-- })
