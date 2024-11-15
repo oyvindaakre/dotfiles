323f8b7996a6f10085756693c64fc3f5845b5b35
Øyvind Aarrestad Aakre <oyvind.aakre@easee.com>
2024-11-15 13:14:42 +0100

    [lsp] install clangd by default

--- config/nvim/lua/plugins/lsp.lua
+++ config/nvim/lua/plugins/lsp.lua
@@ -197,6 +197,7 @@ return { -- LSP Configuration & Plugins
     local ensure_installed = vim.tbl_keys(servers or {})
     vim.list_extend(ensure_installed, {
       "stylua", -- Used to format Lua code
+      { "clangd", version = "18.1.3" },
     })
     require("mason-tool-installer").setup({ ensure_installed = ensure_installed })
 
