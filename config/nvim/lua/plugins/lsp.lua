c430e2a5f7c65b596364ef3e477799abe6689568
Øyvind Aarrestad Aakre <oyvind.aakre@easee.com>
2024-11-19 10:37:48 +0100

    Lock cortex-debug version to 1.12.1

--- config/nvim/lua/plugins/lsp.lua
+++ config/nvim/lua/plugins/lsp.lua
@@ -196,8 +196,8 @@ return { -- LSP Configuration & Plugins
     local ensure_installed = vim.tbl_keys(servers or {})
     vim.list_extend(ensure_installed, {
       "stylua", -- Used to format Lua code
-      "cortex-debug",
       { "clangd", version = "19.1.0" },
+      { "cortex-debug", version = "1.12.1" },
     })
     require("mason-tool-installer").setup({ ensure_installed = ensure_installed })
 
