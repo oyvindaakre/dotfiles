e72a5d7ce74770b2013fe8fa1bf7d7ea69beeeb4
Øyvind Aarrestad Aakre <oyvind.aakre@easee.com>
2026-01-26 10:15:03 +0100

    Fix vim highlight warning in autocmd.lua

--- nvim/.config/nvim/lua/config/autocmd.lua
+++ nvim/.config/nvim/lua/config/autocmd.lua
@@ -4,11 +4,11 @@
 --  Try it with `yap` in normal mode
 --  See `:help vim.highlight.on_yank()`
 vim.api.nvim_create_autocmd("TextYankPost", {
-  desc = "Highlight when yanking (copying) text",
-  group = vim.api.nvim_create_augroup("kickstart-highlight-yank", { clear = true }),
-  callback = function()
-    vim.highlight.on_yank()
-  end,
+	desc = "Highlight when yanking (copying) text",
+	group = vim.api.nvim_create_augroup("kickstart-highlight-yank", { clear = true }),
+	callback = function()
+		vim.hl.on_yank()
+	end,
 })
 
 -- vim.api.nvim_create_autocmd({ "BufEnter" }, {
